bf50967192ad1417621cc3d7b8b8fa8fa3213e9a
SpaceDave <davidreyer1@googlemail.com>
2021-02-23 14:50:22 +0100

    First batch of tagged images

--- annotation_builder.py
+++ annotation_builder.py
@@ -4,8 +4,10 @@ import argparse
 import os
 list=[]
 def main():
-    picpath=input('please enter pictures direction as "C:\\folder" format:')
-    annpath = input('please enter annotation file path as "C:\\folder" format:')
+    # picpath=input('please enter pictures direction as "C:\\folder" format:')
+    picpath = "C:\\Users\\Larry\\PycharmProjects\\DVA485\\Image_dataset"
+    # annpath = input('please enter annotation file path as "C:\\folder" format:')
+    annpath = "C:\\Users\\Larry\\PycharmProjects\\DVA485\\annotation.txt"
     for file in os.listdir(picpath):
         list.append(os.path.join(picpath, file))
     for path in list:
